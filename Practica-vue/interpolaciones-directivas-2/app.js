23e36bed5cb69d83586ee2012ef12f2791fb1807
Daniel Marcano <dmarcanor2@gmail.com>
2020-03-20 13:54:11 -0400

    Agregando instancia de vue

--- Practica-vue/interpolaciones-directivas-2/app.js
+++ Practica-vue/interpolaciones-directivas-2/app.js
@@ -1,6 +1,15 @@
 const app = new Vue({
     el: "#app",
     data: {
-        
+        project: "App",
+        version: 0.1,
+        name: "",
+        last_name: "",
+        age: 0,
+        education: {
+            studying: false,
+            level: "",
+            school: ""
+        }
     }
 });
