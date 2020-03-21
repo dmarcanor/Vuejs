6461659f18e22cc1122e3659a05d743802cc1aaf
Daniel Marcano <dmarcanor2@gmail.com>
2020-03-21 18:57:53 -0400

    Refactorizando

--- Practica-vue/eventos/app.js
+++ Practica-vue/eventos/app.js
@@ -1,7 +1,81 @@
 const app = new Vue({
     el: "#app",
     data: {
-        project: "App",
-        version: 0.1
+        project: "Restaurant",
+        version: 0.1,
+
+        menu: [
+            {
+                foodName: "Pollo",
+                price: 100000
+            },
+            {
+                foodName: "Pizza",
+                price: 120000
+            },
+            {
+                foodName: "Ensalada",
+                price: 80000
+            }
+        ],
+
+        order: {
+            food: new Array(),
+            total: 0
+        }
+    },
+    methods: {
+        createFood: function() {
+            this.menu.push(
+                {
+                    foodName: this.menu.foodName,
+                    price: this.menu.price
+                }
+            );
+        },
+
+        addOrder: function(item){
+            var obj = {
+                foodName: item.foodName,
+                price: item.price,
+                quantity: 1
+            }
+
+            if(this.isItemRepeated(item)){
+                return this.getItemRepeated(item).quantity++;
+            }
+
+            this.order.food.push(obj)
+        },
+
+        getItemRepeated: function(item){
+            return this.order.food.find(element => element.foodName === item.foodName);
+        },
+
+        isItemRepeated: function(item){
+            return this.order.food.find(element => element.foodName === item.foodName) != null ? true : false;
+        },
+
+        getTotal: function(){
+            var total=0
+
+            this.order.food.forEach(element => {
+                total += (element.price * element.quantity)
+            });
+
+            return total;
+        },
+
+        submitOrder: function(){
+            alert("Orden enviada")
+            this.setOrderAsEmpty()
+        },
+
+        //MEJORAR
+        setOrderAsEmpty: function(){
+            for(var i=0; i<=app.order.food.length+1; i++){
+                app.order.food.pop()
+            }
+        }
     }
 });
