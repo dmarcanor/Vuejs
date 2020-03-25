392c5bd4b86dee1f364fdad8b48f602ee1c22cc6
Daniel Marcano <dmarcanor2@gmail.com>
2020-03-25 18:33:17 -0400

    Componente para el formulario que agrega nuevo item al menu

---
+++ Practica-vue/webpack/src/AddMenuItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="col">
+        <h3>Agregar item al menu</h3>
+        <form id="menu" @submit.prevent="createFood">
+            <div class="form-group">
+                <label for="" class="form-control-label">Nombre
+                    <input type="text" class="form-control" v-model="menu.foodName" placeholder="Nombre de la comida">
+                </label>
+            </div>
+        
+            <div class="form-group">
+                <label for="" class="form-control-label">Precio
+                    <input type="number" class="form-control" v-model="menu.price" placeholder="Precio">
+                </label>
+            </div>
+            <button class="btn btn-primary">Enviar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default{
+    props: ['menu'],
+    methods: {
+        createFood: function() {
+            this.menu.push(
+                {
+                    foodName: this.menu.foodName,
+                    price: this.menu.price
+                }
+            )
+            this.clearForm()
+        },
+    
+        clearForm: function() {
+            this.menu.foodName = ""
+            this.menu.price = ""
+        }
+    }
+}
+</script>
