401c5b480eebf81201f6c87ac096c3d91f296c21
Daniel Marcano <dmarcanor2@gmail.com>
2020-03-25 16:03:12 -0400

    Elimina comentarios y arregla typo

--- Practica-vue/componentes-2/app.js
+++ Practica-vue/componentes-2/app.js
@@ -116,7 +116,7 @@ const app = new Vue({
     el: "#app",
     data: {
         project: "Restaurant",
-        version: 0.2,
+        version: 2.0,
         menu: [
             {
                 foodName: "Pollo",
@@ -179,63 +179,3 @@ const app = new Vue({
         }
     }
 });
-
-
-
-/*{
-    createFood: function() {
-        this.menu.push(
-            {
-                foodName: this.menu.foodName,
-                price: this.menu.price
-            }
-        )
-        this.clearForm()
-    },
-
-    clearForm: function() {
-        this.menu.foodName = ""
-        this.menu.price = 0
-    },
-
-    addOrder: function(item){
-        var obj = {
-            foodName: item.foodName,
-            price: item.price,
-            quantity: 1
-        }
-
-        if(this.isItemRepeated(item)){
-            return this.getItemRepeated(item).quantity++;
-        }
-
-        this.order.food.push(obj)
-    },
-
-    getItemRepeated: function(item){
-        return this.order.food.find(element => element.foodName === item.foodName);
-    },
-
-    isItemRepeated: function(item){
-        return this.order.food.find(element => element.foodName === item.foodName) != null ? true : false;
-    },
-
-    getTotal: function(){
-        var total=0
-
-        this.order.food.forEach(element => {
-            total += (element.price * element.quantity)
-        });
-
-        return total;
-    },
-
-    submitOrder: function(){
-        alert("Orden enviada")
-        this.setOrderAsEmpty()
-    },
-
-    setOrderAsEmpty: function(){
-        return this.order.food = []
-    }
-}*/
