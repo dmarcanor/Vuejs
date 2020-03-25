c592786bce2a660e8c1b88a56308e757d64be7ee
Daniel Marcano <dmarcanor2@gmail.com>
2020-03-25 18:34:21 -0400

    Componente para el listado del menu

---
+++ Practica-vue/webpack/src/AppMenu.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="col">
+        <template v-if="menu.length">
+            <h3>Menu</h3>
+            <ul>
+                <li is="menu-item" :item="item" v-for="item in menu" @add="$emit('add-2', item)"></li>
+            </ul>
+        </template>
+    </div>
+</template>
+
+<script>
+import MenuItem from './MenuItem.vue'
+export default{
+    components: {
+        'menu-item': MenuItem
+    },
+    props: ['menu']
+}
+</script>
