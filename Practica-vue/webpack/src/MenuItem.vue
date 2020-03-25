739fc8c53e751870451fef91a64b597fad682d5e
Daniel Marcano <dmarcanor2@gmail.com>
2020-03-25 18:33:53 -0400

    Componente para los items del menu

---
+++ Practica-vue/webpack/src/MenuItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <li v-on:click="add(item)">
+        Nombre: <a>{{item.foodName}}</a> / Precio: {{item.price}}
+    </li>
+</template>
+
+<script>
+export default{
+    props: ['item'],
+    methods: {
+        add: function(item){
+            this.$emit('add', item)
+        }
+    }
+}
+</script>
