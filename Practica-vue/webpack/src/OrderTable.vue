dc9af6bf0dceaed6177210d0882bb23bb5f36112
Daniel Marcano <dmarcanor2@gmail.com>
2020-03-25 18:35:28 -0400

    Componente para la tabla de la orden

---
+++ Practica-vue/webpack/src/OrderTable.vue
@@ -0,0 +1,37 @@
+<template>
+    <table class="table table-bordered">
+        <thead>
+            <th>Nombre</th>
+            <th>Cantidad</th>
+            <th>Precio</th>
+        </thead>
+        <tbody>
+            <tr is="order-item" v-for="item in order.food" :item="item"></tr>
+            <tr>
+                <td>Total a pagar</td>
+                <td>{{getTotal()}}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+import OrderItem from './OrderItem'
+export default{
+    components: {
+        'order-item': OrderItem
+    },
+    props: ['order'],
+    methods: {
+        getTotal: function(){
+            var total=0
+    
+            this.order.food.forEach(element => {
+                total += (element.price * element.quantity)
+            });
+    
+            return total;
+        }
+    }
+}
+</script>
