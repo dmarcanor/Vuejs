d9cbca4bc12513c8262966d6b5f10cd80e75c4fa
Daniel Marcano <dmarcanor2@gmail.com>
2020-03-28 16:38:00 -0400

    Obtiene datos de las mesas desde otro archivo
    Agrega router-view para las rutas hijas

--- Practica-vue/vue-router/src/Components/Tables/Tables.vue
+++ Practica-vue/vue-router/src/Components/Tables/Tables.vue
@@ -1,28 +1,41 @@
 <template>
-    <div class="row">
-        <div class="col">
-            <h2>Mesas</h2>
-            <select name="" id="" v-model="selected">
-                <option :value="mesa" v-for="mesa in mesas">{{mesa}}</option>
-            </select>
-            <div class="width-100"></div>
-            <router-link :to="selected+'/order'"><button class="mt-3 btn btn-primary">Aceptar</button></router-link>
+    <div>
+        <div class="row">
+            <div class="col text-center">
+                <h2>Mesas</h2>
+                <select v-model="selected">
+                    <option :value="table" v-for="table in tables">{{table.number}}</option>
+                </select>
+                <div class="width-100"></div>
+                <router-link v-if="selected" :to="getURL()"><button class="mt-3 btn btn-primary">Aceptar</button></router-link>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <router-view></router-view>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import TablesList from '../../Store/tables'
+
 export default {
     data() {
         return {
-            mesas: [1,2,3,4,5,6,7,8,9,10],
-            selected: 0
+            tables: TablesList.state.tables,
+            selected: null
         }
     },
 
     methods: {
-        getSelected(item) {
-            this.selected = item
+        isBusy() {
+            return this.selected.busy ? "fallido" : "enviado"
+        },
+
+        getURL() {
+            return "/mesa/" + this.isBusy() + "/" + this.selected.number
         }
     }
 }
