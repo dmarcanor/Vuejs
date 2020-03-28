d741ea518c60ed8f15a355ebf02a8ba7d15adfdd
Daniel Marcano <dmarcanor2@gmail.com>
2020-03-28 16:43:01 -0400

    Componente para cuando el envio de la orden es exitosa

---
+++ Practica-vue/vue-router/src/Components/Order/Success.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <hr>
+        <h2>Orden enviada correctamente a la mesa {{id}}</h2>
+    </div> 
+</template>
+
+<script>
+    export default {
+        props: ['id']
+    }
+</script>
