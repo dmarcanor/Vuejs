23a6c512daed22028b91f0129145c9d8545a9827
Daniel Marcano <dmarcanor2@gmail.com>
2020-03-28 16:41:50 -0400

    Carpeta para archivos con datos

---
+++ Practica-vue/vue-router/src/Store/tables.js
@@ -0,0 +1,24 @@
+let tables = [
+    {
+        number: 1,
+        busy: false
+    },
+    {
+        number: 2,
+        busy: false
+    },
+    {
+        number: 3,
+        busy: true
+    },
+    {
+        number: 4,
+        busy: false
+    }
+]
+
+export default {
+    state: {
+        tables //tables: tables
+    }
+}
