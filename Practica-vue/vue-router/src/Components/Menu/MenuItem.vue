4d5e1151b97cea21f9451023ca380332d0601ca2
Daniel Marcano <dmarcanor2@gmail.com>
2020-03-28 16:31:50 -0400

    Elimina metodo inutil

--- Practica-vue/vue-router/src/Components/Menu/MenuItem.vue
+++ Practica-vue/vue-router/src/Components/Menu/MenuItem.vue
@@ -1,16 +1,11 @@
 <template>
-        <li @click="$emit('selected', item)">
-            Nombre: {{item.foodName}} / Precio: {{item.price}}
-        </li>
-    </template>
+    <li @click="$emit('selected', item)">
+        Nombre: {{item.foodName}} / Precio: {{item.price}}
+    </li>
+</template>
     
-    <script>
-    export default{
-        props: ['item'],
-        methods: {
-            select: function(item){
-                this.$emit('select', item)
-            }
-        }
-    }
-    </script>
+<script>
+export default{
+    props: ['item']
+}
+</script>
