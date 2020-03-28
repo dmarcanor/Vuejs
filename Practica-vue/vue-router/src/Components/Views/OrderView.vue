e0a2ea3af279a8c41db833adf94170223a445bf8
Daniel Marcano <dmarcanor2@gmail.com>
2020-03-28 16:40:40 -0400

    Carpeta para componentes view

---
+++ Practica-vue/vue-router/src/Components/Views/OrderView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <ul class="nav nav-bar">
+            <router-link tag="li" to="/" active-class="active"><a>Mesas</a></router-link>
+        </ul>
+        
+        <menu-app></menu-app>
+        <order-app></order-app>
+    </div>
+    
+</template>
+
+<script>
+import Order from '../Order/Order'
+import Menu from '../Menu/Menu'
+
+export default {
+    components: {
+        'order-app': Order,
+        'menu-app': Menu
+    },
+    prop: ['id']
+}
+</script>
